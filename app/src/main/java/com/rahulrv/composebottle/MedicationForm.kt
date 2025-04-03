a4d1efa77fadbcde1d08ae440837ac0e3c796fbb
Rahul Rengaramanujam <rahul.rv87@gmail.com>
2025-04-02 18:29:03 -0700

    Added save button

--- app/src/main/java/com/rahulrv/composebottle/MedicationForm.kt
+++ app/src/main/java/com/rahulrv/composebottle/MedicationForm.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -22,6 +23,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -39,6 +41,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -299,6 +302,23 @@ fun MedicationForm() {
                     )
                 }
             )
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .align(Alignment.CenterHorizontally),
+            onClick = {
+                // TODO: validate all input values
+            }
+        ) {
+            Text(
+                text = stringResource(R.string.save),
+                style = MaterialTheme.typography.bodyLarge
+,            )
         }
 
     }
